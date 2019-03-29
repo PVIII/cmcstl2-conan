f956b03c723a0947ca0b137bd7ce619fb3f629f1
Patrick Wang-Freninger <github@freninger.at>
2019-03-29 22:13:49 +0100

    Add docker flag for leak sanitizer under docker

--- build.py
+++ build.py
@@ -4,6 +4,6 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(docker_run_options='--cap-add SYS_PTRACE')
     builder.add_common_builds(pure_c=False)
     builder.run()
